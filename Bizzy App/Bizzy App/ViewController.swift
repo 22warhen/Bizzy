5e135bfad3b98f30310b9db86d408118faffce24
22warhen <65292685+22warhen@users.noreply.github.com>
2020-05-14 16:20:29 -0400

    Add Xcode project

---
+++ Bizzy App/Bizzy App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bizzy App
+//
+//  Created by Henry Warren on 5/14/20.
+//  Copyright © 2020 Bizzy Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
