13009dbf6e94df89338e2dd8da2dbf2d6663932d
h4nky <henrywarren@Henrys-MacBook-Pro.local>
2020-05-16 00:32:45 -0400

    New Xcode project

---
+++ Bizzy/Bizzy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bizzy
+//
+//  Created by Henry Warren on 5/15/20.
+//  Copyright © 2020 Bizzy Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
