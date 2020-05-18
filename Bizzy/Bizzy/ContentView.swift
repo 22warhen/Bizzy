8d1a4dc403981cc2a04dfc9e09f4286ee9e8668a
h4nky <henrywarren@Henrys-MacBook-Pro.local>
2020-05-18 14:51:10 -0400

    Working on Menu

--- Bizzy/Bizzy/ContentView.swift
+++ Bizzy/Bizzy/ContentView.swift
@@ -7,15 +7,54 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State var open = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack{
+                HStack {
+                    Spacer()
+                    Button(action: {self.open.toggle()}){
+                        Image(systemName: "line.horizontal.3")
+                            .imageScale(.large)
+                            .foregroundColor(.black)
+                    }
+                }
+                Spacer()
+            }.padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        //ContentView()
+        Menu(open: .constant(true))
     }
+}
+
+struct Menu: View {
+    @Binding var open: Bool
+    var body: some View {
+        VStack{
+            HStack{
+                Image(systemName: "gear")
+                    .foregroundColor(.white)
+                        .font(.system(size: 24, weight: .heavy))
+                .frame(width: 32, height: 32)
+            }
+            .padding(.top,20)
+            HStack{
+                Spacer()
+            }
+            
+            Spacer()
+            
+        }
+        .padding(.vertical,30)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("Blue"), Color("Purple")]), startPoint: .top, endPoint: .bottom))
+        .padding(.trailing,80)
+        .edgesIgnoringSafeArea(.vertical)
+    }
+    
 }
---
+++ Bizzy/Bizzy/Menu.swift
@@ -0,0 +1,9 @@
+//
+//  Menu.swift
+//  Bizzy
+//
+//  Created by Henry Warren on 5/16/20.
+//  Copyright © 2020 Bizzy Inc. All rights reserved.
+//
+
+import SwiftUI
